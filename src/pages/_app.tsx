f32c2280da8a2bbd292f3832fb6a4ed14ca7f4f5
Ayrton Souza <ayrtonsouza@icloud.com>
2021-08-01 00:49:36 -0300

    [:nail_care:] initial chakra configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import { AppProps as Props } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
 
-const MyApp: React.VFC<Props> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+const MyApp: React.VFC<Props> = ({ Component, pageProps }) => (
+  <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
 
-export default MyApp
+export default MyApp;
