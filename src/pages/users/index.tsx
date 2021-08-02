1660278e7cf5533fa981c6ff8db5b17b6b85bd7e
Ayrton Souza <ayrtonsouza@icloud.com>
2021-08-02 17:03:07 -0300

    [:construction:] initial pagination component development

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from '@chakra-ui/react';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
+import { Pagination } from '../../components/Pagination';
 import { Sidebar } from '../../components/Sidebar';
 
 const Dashboard: React.VFC = () => (
@@ -76,6 +77,7 @@ const Dashboard: React.VFC = () => (
             </Tr>
           </Tbody>
         </Table>
+        <Pagination />
       </Box>
     </Flex>
   </Box>
